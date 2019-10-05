0c605173b54fbf958ea531ee6248eadc869ac836
Moriyama Masataka <masataka.312@gmail.com>
2019-10-05 19:22:06 +0900

    トップ　基本構成作成

--- app/src/views/Top.vue
+++ app/src/views/Top.vue
@@ -1,4 +1,35 @@
 <template>
     <div id="p-top">
+        <div id="p-top-contents">
+            <div id="p-top-left">
+                <div id="p-top-left__inner">
+                    <div id="p-top-left-content">
+                        <p class="text">あなたの「好き」をフォローしよう。
+</p>
+                        <p class="text">話題のトピックを追いかけましょう。</p>
+                        <p class="text">Twitterに参加しましょう。</p>
+                    </div>
+                </div>
+            </div>
+            <div id="p-top-right">
+                <div id="p-top-right__inner">
+                    <div id="p-top-right-content">
+                        <TopLoginForm />
+                        <h1 id="p-top-right-ttl">「いま」起きていることを<br>見つけよう</h1>
+                        <CreateAccountForm />
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+<script>
+import TopLoginForm from '../components/organisms/TopLoginForm'
+import CreateAccountForm from '../components/organisms/CreateAccountFrom'
+export default {
+    components: {
+        TopLoginForm,
+        CreateAccountForm
+    }
+}
+</script>
