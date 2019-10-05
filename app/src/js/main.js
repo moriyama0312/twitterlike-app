35264c207f79dd1cf385740d9903eda3a4476e1d
Moriyama Masataka <masataka.312@gmail.com>
2019-10-05 20:58:04 +0900

    scssファイル読み込み

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,6 +3,7 @@ import App from '../App.vue'
 import router from './router/router'
 import store from './store/store'
 import './registerServiceWorker'
+import '@/assets/scss/main.scss'
 
 Vue.config.productionTip = false
 
