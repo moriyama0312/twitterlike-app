9cbdd2867c014846af63679b8f25c6024643d48c
Moriyama Masataka <masataka.312@gmail.com>
2019-10-05 22:48:19 +0900

    各ページに入る前にbodyにid付ける

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -7,6 +7,11 @@ import '@/assets/scss/main.scss'
 
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  document.getElementsByTagName('body')[0].setAttribute('id', to.name)
+  next()
+})
+
 new Vue({
   router,
   store,
