acacce8a78b5b581d49a245bec38595591ac158d
Moriyama Masataka <masataka.312@gmail.com>
2019-10-19 18:24:11 +0900

    db check

--- app/backend/db/dbCheck.js
+++ app/backend/db/dbCheck.js
@@ -3,14 +3,28 @@ import connection from './connection.js';
 export default (userInfo, callback) => {
 	const id = userInfo.id;
 	const pw = userInfo.password;
-	const sql = ``;
+	const sql = `SELECT *
+				FROM user_info
+				WHERE user_info.id = ${id};`;
 
 	connection.query(sql, (err, results, fields) => {
 		if(err) {
 			callback(null, 'mysql error!');
 		}else {
-			callback(results);
+			if(validateDB(results, id, pw)) {
+				callback(results);
+			}else {
+				callback(null, 'validate error!');
+			}
 		}
 	});
 
+}
+
+const validateDB = (data, id, pw) => {
+	if(data.id === id && data.password === pw) {
+		return true;
+	}else {
+		return false;
+	}
 }
