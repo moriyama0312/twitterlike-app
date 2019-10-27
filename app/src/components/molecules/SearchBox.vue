0ebf132d922befc1e7b2c4a8553dd58215204171
Moriyama Masataka <masataka.312@gmail.com>
2019-10-27 18:59:55 +0900

    subcontents 検索窓

---
+++ app/src/components/molecules/SearchBox.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="c-search-box">
+		<form>
+			<input
+				:placeholder="placeholder"
+			>
+		</form>
+	</div>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			placeholder: 'キーワード検索'
+		}
+	}
+}
+</script>
---
+++ app/src/components/organisms/SubContents.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="c-sub-contents">
+		<div id="c-sub-contents__inner">
+			<SearchBox />
+			<RecommendArea />
+		</div>
+	</div>
+</template>
+<script>
+import SearchBox from '@/components/molecules/SearchBox.vue'
+import RecommendArea from '@/components/organisms/RecommendArea.vue'
+export default {
+	components: {
+		SearchBox,
+		RecommendArea
+	}
+}
+</script>
