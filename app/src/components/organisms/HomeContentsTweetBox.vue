0be8f27a1411473299b0083b7eb1aae8fba2045a
Moriyama Masataka <masataka.312@gmail.com>
2019-11-30 00:04:50 +0900

    データの整形部分

--- app/src/components/organisms/HomeContentsTweetBox.vue
+++ app/src/components/organisms/HomeContentsTweetBox.vue
@@ -38,7 +38,7 @@ export default {
 	},
 	methods: {
 		Tweet() {
-			this.tweet_time = setTimeNow()
+			setTimeNow()
 			let contents = {
 				text: this.textValue,
 				images: {
@@ -54,7 +54,19 @@ export default {
 			this.textValue = ''
 		},
 		setTimeNow() {
-			
+			let date = new Date()
+			let year = date.getFullYear()
+			let month = shapeData(Number(date.getMonth())+1)
+			let day = shapeData(Number(date.getDate()))
+			let hour = shapeData(Number(date.getHours()))
+			let min = shapeData(Number(date.getMinutes()))
+			let sec = shapeData(Number(date.getSeconds()))
+			this.tweetTime = `${year} + '-' + ${month} + '-' + ${day} + ' ' + ${hour} + ':' + ${min} + ':' + ${sec}`
+		},
+		shapeData(data) {
+			if(data < 10) {
+				
+			}
 		}
 	}
 }
